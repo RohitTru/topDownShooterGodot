b5813b10255700d97514218c03e4b0fbd4dacda7
Rohit <rohit.truesdale@gmail.com>
2024-04-29 09:04:26 -0500

    Added Trees and a mob entity that chases the player

--- player.gd
+++ player.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * 600
+	velocity = direction * 300
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
-		get_node("HappyBoo").play_walk_animation()
+		$HappyBoo.play_walk_animation()
 	else:
-		get_node("HappyBoo").play_idle_animation()
+		$HappyBoo.play_idle_animation()
+	
+
+	
+	
