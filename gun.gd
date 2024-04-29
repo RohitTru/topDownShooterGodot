078b77f42e6d35211d37ec2c518c40d53554c670
Rohit <rohit.truesdale@gmail.com>
2024-04-29 14:03:17 -0500

    mobs now die and perish in smoke

--- gun.gd
+++ gun.gd
@@ -8,7 +8,7 @@ func _physics_process(delta):
 
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
-	var new_bullet = BULLET.instantiate()ww
+	var new_bullet = BULLET.instantiate()
 	new_bullet.global_position = %ShootingPoint.global_position
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
