834c72d2460903252865192dc5de820818f09597
Rohit <rohit.truesdale@gmail.com>
2024-04-29 16:49:23 -0500

    added a health bar to the player

--- mob.gd
+++ mob.gd
@@ -9,7 +9,7 @@ func _ready():
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 170
+	velocity = direction * 125
 	move_and_slide()
 	
 func take_damage():
